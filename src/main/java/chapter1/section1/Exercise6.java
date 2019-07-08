505c613422736694b151ab4b0b6ca27b4eb05a07
Lei Wang <lei.wang@acerta.be>
2019-07-08 17:22:30 +0200

    added junit support, improve code of ch1 ex6

--- src/main/java/chapter1/section1/Exercise6.java
+++ src/main/java/chapter1/section1/Exercise6.java
@@ -2,20 +2,33 @@ package chapter1.section1;
 
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Rene Argento
  */
 public class Exercise6 {
-
-	public static void main(String[] args) {
+	static void originalImpl() {
 		int f = 0;
 		int g = 1;
-		
+
 		for (int i = 0; i <= 15; i++) {
 			StdOut.println(f);
 			f = f + g;
 			g = f - g;
 		}
 	}
-	
+
+	static void alternativeImpl() {
+		List<Integer> fibonacci = new ArrayList<>();
+		fibonacci.add(0);
+		fibonacci.add(1);
+		StdOut.println(fibonacci.get(0));
+		StdOut.println(fibonacci.get(1));
+		for (int i = 2; i <= 15; i++) {
+			fibonacci.add(fibonacci.get(i - 1) + fibonacci.get(i - 2));
+			StdOut.println(fibonacci.get(i));
+		}
+	}
 }
---
+++ src/test/java/chapter1/section1/Exercise6Test.java
@@ -0,0 +1,15 @@
+package chapter1.section1;
+
+import org.junit.Test;
+
+public class Exercise6Test {
+    @Test
+    public void originalImpl() {
+        Exercise6.originalImpl();
+    }
+
+    @Test
+    public void alternativeImpl() {
+        Exercise6.alternativeImpl();
+    }
+}
